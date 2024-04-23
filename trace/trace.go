4779fdb917f294319c7193152829e355eb2b224d
Yuhan Yang <yangfisher1@sjtu.edu.cn>
2024-04-23 19:40:09 +0800

    minor

--- trace/trace.go
+++ trace/trace.go
@@ -330,6 +330,7 @@ func (s *span) EndAndAggregate(w http.ResponseWriter, r *http.Request) {
 				// Check whether the request is valid or not
 				for e := range exp {
 					errType := e.FilterSpan(sd)
+					fmt.Println("errType: ", errType)
 					switch errType {
 					case OK:
 						ssd := makeServerlessSpanData(sd)
@@ -356,6 +357,12 @@ func (s *span) EndAndAggregate(w http.ResponseWriter, r *http.Request) {
 					case Error:
 						// Report the span immediately
 						e.ExportSpan(sd)
+					}
+
+					// Check response header
+					for key, value := range w.Header() {
+						fmt.Println("key: ", key)
+						fmt.Println("value: ", value)
 					}
 				}
 			}
