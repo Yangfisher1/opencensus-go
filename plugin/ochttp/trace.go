3a02a20eb55be0788e1f15b9750c8ac64878a4b5
Yuhan Yang <yangfisher1@sjtu.edu.cn>
2024-04-23 19:48:45 +0800

    problem should be here

--- plugin/ochttp/trace.go
+++ plugin/ochttp/trace.go
@@ -15,6 +15,7 @@
 package ochttp
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptrace"
@@ -97,6 +98,10 @@ func (t *traceTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 		// FIXME: not here, but in the end of the span
 		span.EndAtClient(resp)
 		return resp, err
+	}
+
+	for key, value := range resp.Header {
+		fmt.Println("key: ", key, "value: ", value)
 	}
 
 	span.AddAttributes(responseAttrs(resp)...)
